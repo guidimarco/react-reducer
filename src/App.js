2d06c1a07ab3024ed1f6ae5f49fc21f9a6a04c75
Marco Guidi <marco.guidi@vola.it>
2024-07-11 12:01:45 +0200

    prj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import './styles.css';
+import { useReducer } from 'react';
+import TicketForm from './components/TicketForm';
+import TicketList from './components/TicketList';
+import ticketReducer from './reducers/ticketReducer';
+import {sortTicket} from "./utilities/sortingUtilities";
 
 function App() {
+
+  const initialState = {
+    tickets: [],
+    editingTicket: null,
+    sortPreference: "High to Low"
+  }
+
+  const [state, dispatch] = useReducer(ticketReducer, initialState)
+
+  const sortedTickets = sortTicket(state.tickets, state.sortPreference)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <h1>Bug Blaster</h1>
+
+        <TicketForm dispatch={dispatch} editingTicket={state.editingTicket} />
+
+        {state.tickets.length > 0 && (
+          <div className='results'>
+            <h2>All tickets</h2>
+
+            <select value={state.sortPreference} onChange={ev => dispatch({type: "SET_SORTING", payload: ev.target.value})}>
+              <option value="High to Low">High to Low</option>
+              <option value="Low to High">Low to High</option>
+            </select>
+
+            <TicketList tickets={sortedTickets} dispatch={dispatch} />
+          </div>
+        )}
+        
+      </div>
     </div>
   );
 }
---
+++ src/components/TicketForm.js
@@ -0,0 +1,101 @@
+import React, {useState, useEffect} from "react";
+
+export default function TicketForm({dispatch, editingTicket}) {
+
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [priority, setPriority] = useState('1')
+
+  useEffect(() => {
+    if (editingTicket) {
+      setTitle(editingTicket.title)
+      setDescription(editingTicket.description)
+      setPriority(editingTicket.priority)
+    } else {
+      clearForm()
+    }
+  }, [editingTicket])
+
+  const priorityLabels = {
+    1: 'Low',
+    2: 'Medium',
+    3: 'High'
+  }
+
+  const clearForm = () => {
+    setTitle('')
+    setDescription('')
+    setPriority('1')
+  }
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+
+    const ticketData = {
+      id: editingTicket ? editingTicket.id : new Date().toISOString(),
+      title,
+      description,
+      priority
+    }
+
+    dispatch({
+      type: editingTicket ? "UPDATE_TICKET" : "ADD_TICKET",
+      payload: ticketData
+    })
+
+    clearForm()
+  }
+
+  const handleCancel = () => {
+    dispatch({type: "CLEAR_EDITING_TICKET"})
+    clearForm()
+  }
+
+  return(
+    <form onSubmit={handleSubmit} className="ticket-form">
+      <div>
+        <label>Title</label>
+        <input 
+          type="text" 
+          value={title} 
+          className="form-input" 
+          onChange={ev => setTitle(ev.target.value)}
+        ></input>
+      </div>
+
+      <div>
+        <label>Description</label>
+        <textarea
+          type="text" 
+          value={description} 
+          className="form-input" 
+          onChange={ev => setDescription(ev.target.value)}          
+        ></textarea>
+      </div>
+
+      <fieldset className="priority-fieldset">
+        <legend>Priority</legend>
+        {
+          Object.entries(priorityLabels).map(([value, label]) => (
+            <label key={value} className="priority-label">
+              <input 
+                type="radio" 
+                value={value} 
+                checked={priority === value} 
+                className="priority-input"
+                onChange={(ev) => setPriority(ev.target.value)}
+              ></input>
+              {label}
+            </label>
+          ))
+        }
+      </fieldset>
+
+      <button type="submit" className="button">Submit</button>
+
+      {editingTicket && (
+        <button className="button" onClick={handleCancel}>Cancel edit</button>
+      )}
+    </form>
+  )
+}
